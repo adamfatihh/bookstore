12b73bd5da2f172d973b32daee2d86b9fe1a0808
adamfatih <bia124@myy.haaga-helia.fi>
2026-09-07 18:27:41 +0300

    Tervetulosivu tehty

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,4 +1,4 @@
-package com.web;
+package com.example.bookstore.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,3 +12,4 @@ public class BookController {
     }
     
 }
+
